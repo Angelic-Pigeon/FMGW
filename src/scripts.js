1638d4a3657604c269951ca3d68cb7b8115254ca
Aleksandar Angelov <71765901+AleksandarAngelov8@users.noreply.github.com>
2023-09-24 21:22:20 +0300

    addedlogosbuttonsheadernospacesinbetween

--- src/scripts.js
+++ src/scripts.js
@@ -1,4 +1,10 @@
-function myFunction()
-{
+function myFunction() {
 	document.getElementById("testID").innerHTML = "This for sure works Copium";
+}
+function goToHome() {
+	window.open("index.html", "_self");
+}
+function goToTournaments()
+{
+	window.open("tourney_page.html", "_self");
 }
