249c3079f37745fd93d3847a5b18863186765808
Aleksandar Angelov <71765901+AleksandarAngelov8@users.noreply.github.com>
2023-09-24 21:27:53 +0300

    fuckyopu

---
+++ scripts.js
@@ -0,0 +1,10 @@
+function myFunction() {
+	document.getElementById("testID").innerHTML = "This for sure works Copium";
+}
+function goToHome() {
+	window.open("index.html", "_self");
+}
+function goToTournaments()
+{
+	window.open("tourney_page.html", "_self");
+}
