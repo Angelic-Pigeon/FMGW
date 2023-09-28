2c9776ccbfa8bc8598aa618b785fb99e8a8f09f5
Aleksandar Angelov <71765901+AleksandarAngelov8@users.noreply.github.com>
2023-09-28 12:08:19 +0300

    added table buttons, one works to redirect to the poker game

--- JS/scripts.js
+++ JS/scripts.js
@@ -24,5 +24,18 @@ function clickLogoutBtn() {
 function dimFix(element) {
 	var position = document.getElementById(element).position;
 	console.log(position);
-	
+}
+function goToTableSelect() {
+	window.open("table_select.html","_self");
+}
+function isThereAnAstolfo()
+{
+	if (/*here we can add a check for the number of players in a certain table
+		if it's more than 0 then this needs to be*/true) {
+			document.getElementsByClassName("gameTable")[0].src = "img/tableA.png";
+	}
+	/*also this needs to be done with the other tables (most likely not the way i've done it hehe')*/
+}
+function openPokerGame() {
+	window.open("poker.html","_self");
 }
--- README.md
+++
@@ -1,2 +0,0 @@
-# FMGW
-Femboy-Made Gambling Website
